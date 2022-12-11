94987b6c14971464b2314b0f360520044af7882b
Khapisov Malik <81933036+Knigan@users.noreply.github.com>
2022-12-11 23:12:12 +0300

    Add files via upload

--- MS7.py
+++ MS7.py
@@ -51,66 +51,77 @@ def Dm(sample):
 def quantile(D, alpha):
     return D[math.floor((1 - alpha) * m) - 1]
 
+def Dmprint(sample):
+    X = sample()
+    print()
+    print(X[:10])
+
+    minX = min(X)
+    M = max(X)
+    print("Крайние члены вариационного ряда: min(X) = %.5f, max(X) = %.5f" % (minX, M))
+    w = M - minX
+    print("Размах выборки составляет w = %.5f" % w)
+
+    l = 1 + math.trunc(math.log2(n))
+    h = w / l
+    print("Количество интервалов группировки l: %d" % l)
+    print("Интервальный шаг h = %.5f" % h)
+
+    Int = [minX + i * h for i in range(l + 1)]
+
+    hist = np.histogram(X, l)
+
+    freq = [hist[0][i] for i in range(l)]
+
+    relfreq = [freq[i] / n for i in range(l)]
+
+    density = [relfreq[i] / h for i in range(l)]
+
+    print()
+
+    print("Интервал:", end = " " * 24)
+    for i in range(l):
+        print('|', str("[%.5f; %.5f)" % (Int[i], Int[i + 1])), '|', sep = ' ', end = ' ' * 2)
+
+    print()
+
+    print("Частоты:", end = " " * 25)
+
+    for i in range(l):
+        print('|', str(freq[i]).center(18), '|', end = ' ' * 2)
+
+    print()
+
+    print("Относительные частоты:", end = " " * 11)
+
+    for i in range(l):
+        print('|', str("%.5f" % relfreq[i]).center(18), '|', end = ' ' * 2)
+
+    print()
+
+    print("Плотность относительной частоты:", end=" ")
+    for i in range(l):
+        print('|', str("%.5f" % density[i]).center(18), '|', end = " " * 2)
+
+    print()
+
+    print("D = %.5f" % D1(Int, freq))
+    print()
+
+    return D1(Int, freq)
+
+Dmprint(sample2)
+
 D = [Dm(sample2) for i in range(m)]
 D.sort()
 
-X = sample2()
+print("Квантиль уровня 0.1 равна %.5f" % quantile(D, 01.1))
+print("Квантиль уровня 0.05 равна %.5f" % quantile(D, 0.05))
+print("Квантиль уровня 0.01 равна %.5f" % quantile(D, 0.01))
 
-minX = min(X)
-M = max(X)
-print("Крайние члены вариационного ряда: min(X) = %.5f, max(X) = %.5f" % (minX, M))
-w = M - minX
-print("Размах выборки составляет w = %.5f" % w)
-
-l = 1 + math.trunc(math.log2(n))
-h = w / l
-print("Количество интервалов группировки l: %d" % l)
-print("Интервальный шаг h = %.5f" % h)
-
-Int = [minX + i * h for i in range(l + 1)]
-
-hist = np.histogram(X, l)
-
-freq = [hist[0][i] for i in range(l)]
-
-relfreq = [freq[i] / n for i in range(l)]
-
-density = [relfreq[i] / h for i in range(l)]
+print("D (A = 0) = %.5f" % Dmprint(sample0))
+print("D (A = 1) = %.5f" % Dmprint(sample1))
 
 print()
-
-print("Интервал:", end = " " * 24)
-for i in range(l):
-    print('|', str("[%.5f; %.5f)" % (Int[i], Int[i + 1])), '|', sep = ' ', end = ' ' * 2)
-
-print()
-
-print("Частоты:", end = " " * 25)
-
-for i in range(l):
-    print('|', str(freq[i]).center(18), '|', end = ' ' * 2)
-
-print()
-
-print("Относительные частоты:", end = " " * 11)
-
-for i in range(l):
-    print('|', str("%.5f" % relfreq[i]).center(18), '|', end = ' ' * 2)
-
-print()
-
-print("Плотность относительной частоты:", end=" ")
-for i in range(l):
-    print('|', str("%.5f" % density[i]).center(18), '|', end = " " * 2)
-
-print()
-
-print("D (A = 2) = %.5f" % D1(Int, freq))
-print()
-
-print("Квантиль уровня 0.9 равна %.5f" % quantile(D, 0.1))
-print("Квантиль уровня 0.95 равна %.5f" % quantile(D, 0.05))
-print("Квантиль уровня 0.99 равна %.5f" % quantile(D, 0.01))
-
-print("D (A = 0) = %.5f" % Dm(sample0))
-print("D (A = 1) = %.5f" % Dm(sample1))
+print(D[:10])
+print(D[-10:])
