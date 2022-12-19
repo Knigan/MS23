1f9b7e7e0a2d83538066bfd4e354cb1c9ebc82e9
Khapisov Malik <81933036+Knigan@users.noreply.github.com>
2022-12-19 22:20:01 +0300

    Add files via upload

--- MS7.py
+++ MS7.py
@@ -28,8 +28,8 @@ def sample2():
 def SetP(a, b):
     return (2 * b - b * b) - (2 * a - a * a)
 
-def D1(Int, freq):
-    Sets = [(Int[i], Int[i + 1], freq[i]) for i in range(len(freq))]
+def D1(I, F):
+    Sets = [(I[i], I[i + 1], F[i]) for i in range(len(F))]
     return 1 / n * max([abs(Sets[j][2] - n * SetP(Sets[j][0], Sets[j][1])) for j in range(len(Sets))])
 
 l = 1 + math.trunc(math.log2(n))
@@ -50,8 +50,8 @@ def Dm(sample):
 
     return D1(Int, freq)
 
-def quantile(D, alpha):
-    return D[math.floor((1 - alpha) * m) - 1]
+def quantile(arr, alpha):
+    return arr[math.floor((1 - alpha) * m) - 1]
 
 def Dmprint(sample):
     X = sample()
@@ -99,6 +99,7 @@ def Dmprint(sample):
         print('|', str("%.5f" % density[i]).center(18), '|', end = " " * 2)
 
     print()
+    print()
 
     print("D = %.5f" % D1(Int, freq))
     print()
@@ -107,13 +108,26 @@ def Dmprint(sample):
 
 
 Dmprint(sample2)
+print()
+
+th = [SetP(i / l, (i + 1) / l) for i in range(l)]
+print("Теоретические вероятности: ", th)
+
+nth = [n * th[i] for i in range(l)]
+print("n*p_j: ", nth)
+
+print()
 
 D = [Dm(sample2) for i in range(m)]
 D.sort()
 
-print("Квантиль уровня 0.1 равна %.5f" % quantile(D, 01.1))
-print("Квантиль уровня 0.05 равна %.5f" % quantile(D, 0.05))
-print("Квантиль уровня 0.01 равна %.5f" % quantile(D, 0.01))
+print("Квантиль уровня 0.1 равна %.10f" % quantile(D, 0.1))
+print("Квантиль уровня 0.05 равна %.10f" % quantile(D, 0.05))
+print("Квантиль уровня 0.01 равна %.10f" % quantile(D, 0.01))
+
+print()
+
+print()
 
 print("D (A = 0) = %.5f" % Dmprint(sample0))
 print("D (A = 1) = %.5f" % Dmprint(sample1))
@@ -124,23 +138,30 @@ print(D[-10:])
 
 seaborn.set_style("whitegrid")
 
-IntD = [0.5 * (Int[i] + Int[i + 1]) for i in range(l)]
+l1 = 1 + math.trunc(math.log2(m))
+print("Количество интервалов группировки l: %d" % l1)
 
-hist = np.histogram(D, l)
+h1 = 1 / l1
+print("Интервальный шаг h = %.5f" % h1)
 
-freq = [hist[0][i] for i in range(l)]
+Int1 = [i * h1 for i in range(l1 + 1)] 
 
-relfreq = [freq[i] / m for i in range(l)]
+IntD = [0.5 * (Int1[i] + Int1[i + 1]) for i in range(l1)]
 
-density = [relfreq[i] / h for i in range(l)]
+hist = np.histogram(D, l1)
 
+freq = [hist[0][i] for i in range(l1)]
 
-plt.bar(IntD, relfreq, width = h, color="navy")
+relfreq = [freq[i] / m for i in range(l1)]
+
+density = [relfreq[i] / h1 for i in range(l1)]
+
+plt.bar(IntD, relfreq, width = h1, color="navy")
 plt.xlabel("int")
 plt.ylabel("Относительные частоты")
 plt.show()
 
-plt.bar(IntD, density, width = h, color="navy")
+plt.bar(IntD, density, width = h1, color="navy")
 plt.xlabel("int")
 plt.ylabel("Плотность относительных частот")
 plt.show()
